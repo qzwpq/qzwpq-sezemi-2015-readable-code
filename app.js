9921ef2fb5b2e27f8d5e91a8bc89f9b8880d8379
qzwpq <qzwpqpwq@gmail.com>
2015-06-06 11:25:33 +0900

    spec4: 仕様4を実装
    *READMEにレシピの追加方法を追記

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 
 var fs = require('fs');
 
-var readJson = function(path){
-	var data = fs.readFileSync(path,{encoding:'utf8'});
+var readJson = function(path) {
+	var data = fs.readFileSync(path, {
+		encoding: 'utf8'
+	});
 	return JSON.parse(data);
 };
 
-var recipes=readJson('./recipe.json');
+var recipes = readJson('./recipe.json');
 
-recipes.forEach(function(recipe){
+recipes.forEach(function(recipe) {
 	console.log(recipe.name);
 });
