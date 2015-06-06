ea069b11db3995399a4d4b7da007c317db28ed05
qzwpq <qzwpqpwq@gmail.com>
2015-06-06 11:37:59 +0900

    spec5: 仕様5を実装
    *名前からIDを返す関数を追加

--- app.js
+++ app.js
@@ -9,8 +9,25 @@ var readJson = function(path) {
 	return JSON.parse(data);
 };
 
+var initIdTable = function() {
+	var table = {};
+	var count = 0;
+	return function(name) {
+		// confirm the name is not in the table
+		if (table[name] === void 0) {
+			return ++count;
+		} else {
+			return count;
+		}
+	};
+}
+
+var getRecipeId = initIdTable();
+
 var recipes = readJson('./recipe.json');
 
 recipes.forEach(function(recipe) {
-	console.log(recipe.name);
+	var recipeName = recipe.name;
+	var recipeId = getRecipeId(recipeName);
+	console.log(recipeId + ': ' + recipeName);
 });
--- memo.md
+++ memo.md
@@ -1,2 +1,3 @@
 * 外部ファイルは扱いやすいjsonで
 * ただのfor文よりforEachのほうが読みやすい
+https://github.com/45deg/45deg-sezemi-2015-readable-code/blob/08850d2af205455981788e005a2062f92f667382/app.js
