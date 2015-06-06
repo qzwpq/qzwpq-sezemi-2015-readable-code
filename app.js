f4d48c368a07644d793bfbaa06c8158725cf7b17
qzwpq <qzwpqpwq@gmail.com>
2015-06-06 12:03:47 +0900

    spec6: work in progress

--- app.js
+++ app.js
@@ -20,14 +20,38 @@ var initIdTable = function() {
 			return count;
 		}
 	};
-}
+};
+
+var getNameById = function(id, obj) {
+	var keys = Object.keys(obj);
+	for (var i = keys.length - 1; i >= 0; i--) {
+		var key = keys[i];
+		if (obj[key] === id) {
+			return key;
+		}
+	}
+	new Error('the name is not in the object');
+};
+
+var printIdAndName = function(recipe) {
+	var recipeName = recipe.name;
+	var recipeId = getRecipeId(recipeName);
+	console.log(recipeId + ': ' + recipeName);
+};
+
+var args = process.argv.slice(2);
 
 var getRecipeId = initIdTable();
 
 var recipes = readJson('./recipe.json');
 
-recipes.forEach(function(recipe) {
-	var recipeName = recipe.name;
-	var recipeId = getRecipeId(recipeName);
-	console.log(recipeId + ': ' + recipeName);
-});
+if (args.length === 0) {
+	recipes.forEach(function(recipe) {
+		printIdAndName(recipe);
+	});
+} else {
+	args.forEach(function(id) {
+		var var name=getNameById(id);
+		printIdAndName(recipe);
+	});
+}
