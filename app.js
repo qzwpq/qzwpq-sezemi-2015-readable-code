c838445cd298a8cb6925a33812900cf9b6566c5b
qzwpq <qzwpqpwq@gmail.com>
2015-06-06 11:12:53 +0900

    spec3: spec3を実装
    *レシピデータを外部ファイル(json)に分離
    *jsonファイルを読み込む関数を追加

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('オムライス');
+
+var fs = require('fs');
+
+var readJson = function(path){
+	var data = fs.readFileSync(path,{encoding:'utf8'});
+	return JSON.parse(data);
+};
+
+var recipes=readJson('./recipe.json');
+
+recipes.forEach(function(recipe){
+	console.log(recipe.name);
+});
